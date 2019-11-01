284355ca1d98a97ee5313191509b7c02fd4779be
Victor <vcarvalho0402@gmail.com>
2019-11-01 17:58:24 -0300

    Fix the InputText component name

--- src/components/TextInput/index.tsx
+++ src/components/TextInput/index.tsx
@@ -3,7 +3,7 @@ import style from './TextInput.module.scss'
 
 type TextInputProps = { name: string; value?: string }
 
-const Index = ({ name, value }: TextInputProps) => (
+const TextInput = ({ name, value }: TextInputProps) => (
   <input
     className={style['text-input']}
     type="text"
@@ -12,4 +12,4 @@ const Index = ({ name, value }: TextInputProps) => (
   />
 )
 
-export default Index
+export default TextInput
