c0ee83dff7e6d612111bae57932ee9bcf3b5d9ec
Victor <vcarvalho0402@gmail.com>
2019-11-12 16:21:41 -0300

    Adds react-docgen-typescript to generate docs based on the Interfaces
    react-styleguidist doesn't understand typescript, so this parser is
    necessary in order to generate the "Props & Methods" section based on
    the Interfaces used in the components.

--- styleguide.config.js
+++ styleguide.config.js
@@ -1,4 +1,37 @@
+// When some property is optional, react-docgen-typescript adds an "undefined" option to the docs.
+// This custom propsParser removes these unnecessary "undefined".
+const propsParser = (filePath, source, resolver, handlers) => {
+  const result = require('react-docgen-typescript').withCustomConfig(
+    './tsconfig.json',
+    {
+      propFilter: {},
+    },
+  ).parse(filePath, source, resolver, handlers);
+
+  return result.map(component => {
+    const {props} = component;
+    const mappedProps = Object.values(props).reduce((previous, prop) => {
+      const {name} = prop;
+      return {
+        ...previous,
+        [name]: {
+          ...prop,
+          type: {
+            name: prop.type.name.replace('| undefined', ''),
+          },
+        },
+      };
+    }, {});
+
+    return {
+      ...component,
+      props: mappedProps,
+    };
+  });
+};
+
 module.exports = {
+  propsParser,
   sections: [
     {
       name: 'AY Design Library',
