ffd10b959366e7528e331f9c5b99853ab785ca14
Victor <vcarvalho0402@gmail.com>
2019-12-03 09:55:01 -0300

    Export the Card component

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 export { default as Input } from './components/Input';
 export { default as Button } from './components/Button';
+export { default as Card } from './components/Card';
 
 export * from './components/Grid';
 export * from './components/Sidebar';
