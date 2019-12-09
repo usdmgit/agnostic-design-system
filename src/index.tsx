d3631ffad336ee66ae830ad8216bc5587b2b564d
Victor <vcarvalho0402@gmail.com>
2019-12-09 10:29:58 -0300

    Export the Text component in the prod build

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 export { default as Input } from './components/Input';
 export { default as Button } from './components/Button';
 export { default as Card } from './components/Card';
+export { default as Text } from './components/Text';
 
 export * from './components/Grid';
 export * from './components/Sidebar';
