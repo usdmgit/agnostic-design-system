11d597f865e7478b201a9f66f52eb9b33230ba2d
Rheniery <rhenierymendes@gmail.com>
2020-12-07 12:18:01 -0300

    Create variableClassName property for the Grid component

--- src/components/Grid/Container/index.tsx
+++ src/components/Grid/Container/index.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import styles from '../Grid.css';
+import classNames from 'classnames';
 
 interface PropTypes {
   children?: React.ReactNode;
+  variablesClassName?: string;
 }
 
 const Container: React.FC<PropTypes> = props => {
-  const { children, ...attributes } = props;
+  const { children, variablesClassName, ...attributes } = props;
 
   return (
-    <div {...attributes} className={styles['codelitt-container']}>
+    <div {...attributes} className={classNames(variablesClassName, styles['codelitt-container'])}>
       {children}
     </div>
   );
