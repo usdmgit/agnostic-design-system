a7f6d1f64677cefc3f820bd0f3f421e53c9bde2f
kaiomagalhaes <me@kaiomagalhaes.com>
2021-03-08 21:19:59 -0300

    export email input

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,11 @@
-export { default as Button } from '@/components/Button';
-export { default as Input } from '@/components/Input';
-export { default as TextArea } from '@/components/TextArea';
-export { default as List } from '@/components/List';
-export { default as Toast } from '@/components/Toast';
 export { default as AddressSearch } from '@/components/AddressSearch';
+export { default as Button } from '@/components/Button';
+export { default as EmailInput } from '@/components/EmailInput';
+export { default as Input } from '@/components/Input';
+export { default as Dropdown } from '@/components/Dropdown';
+export { default as List } from '@/components/List';
 export { default as MapsProvider } from '@/components/MapsProvider';
 export { default as NumericInput } from '@/components/NumericInput';
-export { default as Dropdown } from '@/components/Dropdown';
+export { default as TextArea } from '@/components/TextArea';
+export { default as Toast } from '@/components/Toast';
 export * from '@/components/Grid';
