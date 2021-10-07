340fe085448fa97f117af4fab87219d3695e92a7
paulohfev <phfernandesev@gmail.com>
2021-10-07 20:37:23 -0300

    Fix Numeric Input max validation

--- src/components/NumericInput/index.tsx
+++ src/components/NumericInput/index.tsx
@@ -36,20 +36,29 @@ interface Props {
   withPrependSeparator?: boolean;
 }
 
+const NON_DIGITS_REGEX = new RegExp(/\D+/, 'g');
+
+const isNumberBiggerThanMax = (radix, num, max) => {
+  if (radix) {
+    const splitNumbers = num.split(radix);
+    const removedPlacementsNumber = splitNumbers[0].replace(NON_DIGITS_REGEX, '');
+    const decimalValue = splitNumbers[1];
+    const result = Number(`${removedPlacementsNumber}.${decimalValue || 0}`);
+    return max < result;
+  } else {
+    const numberWithoutRadix = num.replace(NON_DIGITS_REGEX, '');
+    return max < numberWithoutRadix;
+  }
+};
+
 const NumericInput = React.forwardRef<HTMLInputElement, Props>((props, ref) => {
   const { value, scale, max, radix, positive, ...inputProps } = props;
   const SCALE_REGEX = new RegExp(`^.+${radix}\\d{${scale + 1}}`);
 
-  const isNumeric = num => !isNaN(num);
-
-  const filters = [
+  const filters: Filter[] = [
     {
       type: 'RegExp',
       test: /[^\d.,-]/
-    },
-    {
-      type: 'function',
-      test: value => isNumeric(max) && value > max!
     },
     {
       type: 'function',
@@ -58,6 +67,10 @@ const NumericInput = React.forwardRef<HTMLInputElement, Props>((props, ref) => {
     {
       type: 'function',
       test: value => positive && value.lastIndexOf('-') !== -1
+    },
+    {
+      type: 'function',
+      test: value => isNumberBiggerThanMax(radix, value, max)
     }
   ];
 
