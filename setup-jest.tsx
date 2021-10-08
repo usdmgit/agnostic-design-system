9635082902364c6b377a4370f16e7aba05f42a32
Victor <vcarvalho0402@gmail.com>
2021-10-08 11:18:27 -0300

    Upgrade react and storybook

--- setup-jest.tsx
+++ setup-jest.tsx
@@ -1,5 +1,6 @@
 import { configure } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+// @ts-ignore
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 import '@testing-library/jest-dom/extend-expect';
 
 configure({ adapter: new Adapter() });
