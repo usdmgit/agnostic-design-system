b4ad259e23fce2dff44a53721d7177bb70d740bd
raphaelsattler <raphaelsattler@hotmail.com>
2021-11-04 15:55:22 -0300

    Create Tag component

--- src/index.tsx
+++ src/index.tsx
@@ -10,6 +10,7 @@ export { default as MapsProvider } from '@/components/MapsProvider';
 export { default as NumericInput } from '@/components/NumericInput';
 export { default as PhoneInput } from '@/components/PhoneInput';
 export { default as RangeSlider } from '@/components/RangeSlider';
+export { default as Tag } from '@/components/Tag';
 export { default as TextArea } from '@/components/TextArea';
 export { default as Toast } from '@/components/Toast';
 export { default as UploadAvatar } from '@/components/UploadAvatar';
