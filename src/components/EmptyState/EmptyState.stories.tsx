15ea6adb429e37184ad7179f02288ed0b977c818
paulohfev <phfernandesev@gmail.com>
2022-02-08 15:12:24 -0300

    Add EmptyState Component

---
+++ src/components/EmptyState/EmptyState.stories.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import EmptyState from '@/components/EmptyState';
+import TestImage from '@/assets/images/icons/web/gear.svg';
+import mdx from './EmptyState.stories.mdx';
+import { Meta } from '@storybook/react';
+
+export default {
+  title: 'Components/EmptyState',
+  component: EmptyState,
+  parameters: {
+    docs: {
+      page: mdx
+    }
+  }
+} as Meta;
+
+const Template = args => {
+  return <EmptyState {...args} />;
+};
+
+export const Default = Template.bind({});
+Default.args = {
+  description:
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris suscipit viverra aliquam.',
+  title: 'Lorem Ipsum'
+};
+
+export const WithCustomImage = Template.bind({});
+WithCustomImage.args = {
+  description: 'Hello World',
+  image: <TestImage height='100%' width='100%' />,
+  title: 'Hello World'
+};
+
+export const WithElementAsText = Template.bind({});
+WithElementAsText.args = {
+  description: <span>HTML Description</span>,
+  title: <p>HTML Title</p>
+};
