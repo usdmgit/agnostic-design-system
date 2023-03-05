828f8740e8f5a6d92aa60749f7d066257e8f1770
usdmdevgit <j.murray@usdm.com>
2023-03-05 12:21:25 -0800

    Fix bad Copy

--- src/components/EmailInput/RichTextEditor/buttonNameFormatter.ts
+++
@@ -1,7 +0,0 @@
-export default function buttonNameFormatter(str) {
-  return str
-    .toLowerCase()
-    .split(' ')
-    .map(word => word.charAt(0).toUpperCase() + word.substring(1))
-    .join('');
-}
